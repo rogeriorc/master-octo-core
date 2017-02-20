f7b91dfd36e670018424393c9276a5551e135162
Rogerio Ribeiro da Cruz <rogeriorc@gmail.com>
2017-02-20 15:43:13 -0300

    .

--- src/util/releases/master-octo-core-js.js
+++ src/util/releases/master-octo-core-js.js
@@ -6,8 +6,6 @@ let path = require('path'),
 	git = require('totvstec-tools').git,
 	version = require('totvstec-tools').version;
 
-
-
 const GITHUB_PREFIX = 'https://github.com/rogeriorc/',
 	REPO_NAME = 'master-octo-core-js',
 	TARGET_DIR = path.join(__basedir, 'build', 'release', REPO_NAME),
